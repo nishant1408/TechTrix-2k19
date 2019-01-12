373079d3aecebf3c68538143c4ae2b375639f5c1
Nishant Kumar <n1481997@gmail.com>
2019-01-13 01:25:35 +0530

    Update addcollgf.php

--- addcollgf.php
+++ addcollgf.php
@@ -1,21 +1,28 @@
 <?php
 include 'connect.php';
 session_start();
-if(isset($_SESSION['uid']))
+if(isset($_SESSION['uid'])&& ($_SESSION['type'] == 'a' || $_SESSION['type'] == 'r' ))
 {
 $name=$_POST["name"];
 $insert="INSERT INTO college (name) values ('$name')";
 if(mysqli_query($connect,$insert))
 {
 	echo "<font color='green'><b>COLLEGE ADDED SUCCESSFULLY</b></font>";
+	echo "<table id='table'>";
+	echo "<tr>";
+	echo "<td>College</th>";
+	echo "<td>".$name."</td>";
+	echo "</tr>";
+	echo "</table>";
 }
 else
 {
-	echo "<font color='red'><b>".mysqli_error($connect)."</b></font>";
+	echo "<font color='red'><b>".mysqli_error($connect)."</b></font><br>";
 }
 }
 else
 {
-	echo "<script>location.href='login.php'</script>";
+	echo "<script>alert('YOU ARE NOT AN ADMIN OR REGISTRATION DESK MEMBER')</script>";
+	echo "<script>location.href='dashboard.php'</script>";
 }
 ?>
