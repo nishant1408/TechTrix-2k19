d5b44b92230a9b95b472e0e5c2f9771df4c2191f
nishant1408 <43449063+nishant1408@users.noreply.github.com>
2022-03-06 13:31:06 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 
 <?php
-$connect=mysqli_connect('localhost','admin','techtrixdb','');
+$connect=mysqli_connect('database-1.cfj9r4brvcxy.ap-south-1.rds.amazonaws.com','admin','techtrixdb','','3306');
 ?>
